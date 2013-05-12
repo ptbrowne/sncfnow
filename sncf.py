b9ba4706bababc6f7df666cf36d0648e410be650
Patrick Browne <pt.browne@gmail.com>
2013-05-13 00:38:08 +0200

    debug

--- sncf.py
+++ sncf.py
@@ -82,4 +82,4 @@ def get_trains_complete(origin, destination, day, hour):
     return render_template("trains.html", **kw)
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
