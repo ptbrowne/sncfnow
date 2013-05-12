af91f8c9f1db1e0c9ec94091e09013536eb2935f
Patrick Browne <pt.browne@gmail.com>
2013-05-13 00:05:23 +0200

    first commit

---
+++ sncf.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+
+from datetime import datetime, timedelta, date
+from bs4 import BeautifulSoup
+from flask import Flask, redirect, url_for, render_template, request
+
+
+def date_to_sncf_date(date):
+    day  = str(date.day).zfill(2)
+    month = str(date.month).zfill(2)
+    return "%s%s" % (day, month)
+
+def get_trains(origin, destination, date):
+    url = "http://voyages-sncf.mobi/reservation/selectOD.action?selectv4="
+    payload = {
+        'originName': origin,
+        'destinationName': destination,
+        'outwardJourneyDate': date_to_sncf_date(date),
+        'outwardJourneyHour': date.hour,
+        'back': 0
+    }
+    r = requests.post(url, data=payload)
+    soup =  BeautifulSoup(r.text)
+
+    good_journey = lambda journey: len(journey.find_all("span")) == 5
+    journeys = filter(good_journey, soup.find_all("div", class_="bk-dv"))
+
+    res = []
+    for j in journeys:
+        spans = j.find_all("span")
+        print spans
+        res.append((spans[0].text, spans[1].text, spans[3].text))
+    return res
+
+
+app = Flask(__name__)
+@app.route("/<origin>/<destination>")
+def get_trains_orig_dest(origin, destination):
+    date_ = datetime.now() - timedelta(hours=1)
+    url = url_for('get_trains_complete',
+            origin=origin,
+            destination=destination,
+            day=date_.strftime("%d-%m-%Y"),
+            hour=date_.hour)
+
+    return redirect(url)
+
+@app.route("/<origin>/<destination>/<day>/<hour>")
+def get_trains_complete(origin, destination, day, hour):
+    today = date.today()
+    today = datetime(today.year, today.month, today.day)
+    if day == "auj":
+        day = today
+    elif day == "dem":
+        day = today + timedelta(days=1)
+    else:
+        day = datetime.strptime(day, "%d-%m-%Y")
+    date_ = day + timedelta(hours=int(hour.lstrip("0")) )
+    trains = [
+        ("21h44", "30H30", "5"),
+        ("21h44", "30H30", "6"),
+        ("21h44", "30H30", "7"),
+    ]
+    trains = get_trains(origin, destination, date_)
+
+    kw = {}
+    kw["trains"] = trains
+    kw["origin"] = origin
+    kw["destination"] = destination
+    kw["date_"] = date_
+    if "json" in request.args:
+        return json.dumps(trains)
+    return render_template("trains.html", **kw)
+
+
+app.run(debug=True)
---
+++ static/trains.js
@@ -0,0 +1,38 @@
+var updateTrains = function($scope) {
+  $("#waiting").toggle();
+
+  var
+    origin = $("#origin").text(),
+    destination = $("#destination").text(),
+    day = $("#day").text().replace(/\//g,"-"),
+    hour = $("#hour").text();
+
+  console.log(origin, destination, day, hour)
+
+  $.getJSON("/" + origin + "/" + destination + "/" + day + "/" + hour + "?json")
+  .success(function(data) {
+      $scope.$apply(function() {
+        $scope.trains = data;
+      })
+      $("#waiting").toggle();
+  })
+  .error(function(err) {
+    console.log(err.data)
+  })
+}
+updateTrains = _.debounce(updateTrains, 1200)
+
+var trainCtrl = function($scope) {
+    $scope.trains = trains
+    console.log(trains)
+
+
+    $(".editable").keypress(function(event) {
+      if (event.which == 13) {
+        event.preventDefault()
+      }
+      else {
+        updateTrains($scope);
+      }
+    });
+}
